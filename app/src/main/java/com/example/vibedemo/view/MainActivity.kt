f371413c3fa8b497ed1c4a7c96e5845aa2a2cd0c
choi-seoghwan <74610959+choi-seoghwan@users.noreply.github.com>
2020-12-04 20:15:24 +0900

    Get Music, Set Player

--- app/src/main/java/com/example/vibedemo/view/MainActivity.kt
+++ app/src/main/java/com/example/vibedemo/view/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.vibedemo.view
 
+import android.net.Uri
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.example.vibedemo.R
@@ -7,23 +10,24 @@ import com.example.vibedemo.base.BaseKotlinActivity
 import com.example.vibedemo.databinding.ActivityMainBinding
 import com.example.vibedemo.view.fragment.*
 import com.example.vibedemo.viewmodel.MainViewModel
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import com.squareup.picasso.Picasso
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseKotlinActivity<ActivityMainBinding, MainViewModel>() {
     override val layoutResourceId: Int
         get() = R.layout.activity_main
     override val viewModel: MainViewModel by viewModel()
-
+    private lateinit var player: SimpleExoPlayer
     override fun initStartView() {
-        //Main-Layout에서만 사용되므로 View-binding으로 제공
-        viewDataBinding.mainBottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
-            viewModel.selectMenu(menuItem)
-            true
-        }
+
+
     }
 
     override fun initDataBinding() {
-
         viewModel.selectedMenu.observe(this, Observer { menuItem ->
             var fragment : Fragment = TodayFragment()
             when (menuItem.itemId) {
@@ -43,11 +47,55 @@ class MainActivity : BaseKotlinActivity<ActivityMainBinding, MainViewModel>() {
                     fragment = MyFragment()
                 }
             }
-            supportFragmentManager.beginTransaction().replace(binding.fragmentLayout.id, fragment).commit()
+            supportFragmentManager.beginTransaction().replace(viewDataBinding.mainScreen.id, fragment).commit()
+        })
+        viewModel.statePlayer.observe(this,Observer{
+            viewDataBinding.isSlidingUp = it
+            if(it == true)
+                viewDataBinding.mainPlayer.mainPlayerBottom.player = player
+            else
+                viewDataBinding.miniPlayer.controller.player = player
+        })
+        viewModel.musicPlayerLiveData.observe(this, Observer { it ->
+            player = it
+        })
+        // Music Data setting( title, artist, album-image, lyrics, player(main, mini) )
+        viewModel.musicResponseLiveData.observe(this, Observer {
+            it.let { music ->
+                // mini
+                viewDataBinding.miniPlayer.music = music
+                viewDataBinding.mainPlayer.music = music
+
+                Picasso.with(this).load(music.image).into(viewDataBinding.mainPlayer.albumStandard)
+
+                //뮤직 플레이어
+                val mediaSource: MediaSource =
+                    viewModel.buildMediaSource(Uri.parse(music.file), this)
+                player.setMediaSource(mediaSource)
+                player.prepare()
+            }
         })
     }
 
     override fun initAfterBinding() {
+        viewDataBinding.mainBottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
+            viewModel.selectMenu(menuItem)
+            true
+        }
         viewDataBinding.mainBottomNavigation.selectedItemId = R.id.menu_navi_today
+        viewDataBinding.slidingLayout.addPanelSlideListener(object : SlidingUpPanelLayout.PanelSlideListener {
+            override fun onPanelSlide(panel: View?, slideOffset: Float) {}
+            override fun onPanelStateChanged(
+                panel: View?,
+                previousState: SlidingUpPanelLayout.PanelState?,
+                newState: SlidingUpPanelLayout.PanelState
+            ) {
+                viewModel.setStatePlayer(newState)
+            }
+        })
+        viewModel.initPlayer(this.applicationContext)
+        viewModel.getMusicSearch()
+
+
     }
 }
--- app/src/main/java/com/example/vibedemo/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/vibedemo/viewmodel/MainViewModel.kt
@@ -1,10 +1,22 @@
 package com.example.vibedemo.viewmodel
 
+import android.content.Context
+import android.net.Uri
+import android.util.Log
 import android.view.MenuItem
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.vibedemo.base.BaseKotlinViewModel
 import com.example.vibedemo.model.DataModel
+import com.example.vibedemo.model.response.MusicResponse
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainViewModel(private val model: DataModel) : BaseKotlinViewModel() {
 
@@ -15,9 +27,66 @@ class MainViewModel(private val model: DataModel) : BaseKotlinViewModel() {
     val selectedMenu: LiveData<MenuItem>
         get() = _selectedMenu
 
-    fun selectMenu(item : MenuItem) {
+    fun selectMenu(item: MenuItem) {
         if (selectedMenu.value != item) {
             _selectedMenu.postValue(item)
         }
+    }
+
+    private val _statePlayer = MutableLiveData<Boolean>()
+    val statePlayer: LiveData<Boolean>
+        get() = _statePlayer
+
+    fun setStatePlayer(currentState: SlidingUpPanelLayout.PanelState) {
+        // Collapse = False
+        // Expend = True
+        val state = currentState != SlidingUpPanelLayout.PanelState.COLLAPSED
+        if (state != _statePlayer.value)
+            _statePlayer.postValue(state)
+    }
+
+
+    // player
+    private val _musicPlayerLiveData = MutableLiveData<SimpleExoPlayer>()
+    val musicPlayerLiveData: LiveData<SimpleExoPlayer>
+        get() = _musicPlayerLiveData
+
+
+    fun initPlayer(context: Context) {
+        _musicPlayerLiveData.postValue(SimpleExoPlayer.Builder(context).build())
+    }
+
+    fun setPlayer(player: SimpleExoPlayer) {
+        _musicPlayerLiveData.postValue(player)
+    }
+
+    // music
+    private val _musicResponseLiveData = MutableLiveData<MusicResponse>()
+    val musicResponseLiveData: LiveData<MusicResponse>
+        get() = _musicResponseLiveData
+
+    fun getMusicSearch() {
+        addDisposable(
+            model.getData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    it.run {
+                        _musicResponseLiveData.postValue(this)
+                    }
+                }, {
+                    Log.d(TAG, "response error, message : ${it.message}")
+                })
+        )
+    }
+    internal fun buildMediaSource(uri: Uri, context: Context): MediaSource {
+        var userAgent: String = Util.getUserAgent(context, "project_name")
+        return ExtractorMediaSource.Factory(DefaultHttpDataSourceFactory(userAgent))
+            .createMediaSource(
+                uri
+            )
+    }
+    fun onLikeButtonClick() {
+
     }
 }
